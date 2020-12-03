7ed9a11c809d576b730053838d5b3f9450805989
Ahmadreza Samadi <ahmadreza.smdi@gmail.com>
2020-12-03 20:48:04 +0330

    Adding Self connections

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 #import k from user's input
 k = int(input("K?"))
 
+#open the output file
+f = open("output.txt","w")
 #Calculating the pods, servers, core switches, aggregation switches
 
 pods_Count = int(k)
@@ -13,4 +15,9 @@ CoreSwitch_Count = int((k / 2) ** 2)
 AllSwitch_Count = CoreSwitch_Count + EdgeSwitch_Count + AggrSwitch_Count
 Elements_Count = AllSwitch_Count + Servers_Count
 
-print(pods_Count, Servers_Count, EdgeSwitch_Count, AggrSwitch_Count, CoreSwitch_Count, AllSwitch_Count, Elements_Count)
+# print(pods_Count, Servers_Count, EdgeSwitch_Count, AggrSwitch_Count, CoreSwitch_Count, AllSwitch_Count, Elements_Count)
+
+#Each Element has connection to itself, First we add those connections to output
+for i in range(Elements_Count):
+    out = str(i)+ "\t" + str(i) + "\t" +"1" + "\n"
+    f.write(out)
